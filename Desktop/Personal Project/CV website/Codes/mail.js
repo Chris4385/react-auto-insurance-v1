b787ca48db949d6780bf5ff8fc02a74d90e30bc3
Chris4385 <ganboontoh@gmail.com>
2021-02-23 20:26:27 +0800

    add in environmental variables

--- Desktop/Personal Project/CV website/Codes/mail.js
+++ Desktop/Personal Project/CV website/Codes/mail.js
@@ -1,17 +1,18 @@
 const nodemailer = require("nodemailer");
 const mailGun = require("nodemailer-mailgun-transport");
+require('dotenv').config();
 
 const auth = {
     auth:{
-        api_key:"464e602943f183f491f45430e843ff54-77751bfc-d5bad278",
-        domain:"sandbox73c7e7f2915f415281b032411e833aa2.mailgun.org"
+        api_key:process.env.API,
+        domain:process.env.DOMAIN
     }
 };
 const sendMail = function(sender,senderEmail, subject, message,cb){
     const transporter = nodemailer.createTransport(mailGun(auth));
     const mailOptions = {
         from: senderEmail,
-        to:'ganboontoh@gmail.com',
+        to:process.env.EMAIL,
         subject:subject,
         text: message +" sent from " + sender
       };
